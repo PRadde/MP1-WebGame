9a3f8b263ea9f367ab731389be4282f4ba69683e
PRadde <prestonradde@gmail.com>
2022-09-11 12:57:56 -0500

    last shop and end game button added

--- code.js
+++ code.js
@@ -1,14 +1,18 @@
-var clicks = 9000;
+var clicks = 100250;
 var multiplier = 1;
 var lowShopCost = 101;
 var plusShopOneCost = 525;
 var plusAdderOne = 0;
+var plusShopTwoCost = 10000;
+var plusAdderTwo = 0;
 var multiPlusCost = 3000;
 var multiPlus = 0;
 
+var endGame = 1;
+
 //adds to the click counter 
 function onClick() {
-        clicks += ((multiplier*(multiPlus+1))+plusAdderOne);
+        clicks += endGame*((multiplier*(multiPlus+1))+plusAdderOne+plusAdderTwo);
         document.getElementById("clicks").innerHTML = clicks;
 };
 
@@ -45,5 +49,23 @@ function multiPlusFunc() {
         document.getElementById("multiPlusCost").innerHTML = multiPlusCost;
         document.getElementById("clicks").innerHTML = clicks;
         document.getElementById("multiPlus").innerHTML = multiPlus;
+    }
+}
+
+function plusShopTwo() {
+    if (clicks >= plusShopTwoCost) {
+        clicks -= plusShopTwoCost;
+        plusAdderTwo += 50;
+        plusShopTwoCost = 10000+(plusAdderTwo*100);
+        document.getElementById("plusShopTwoCost").innerHTML = plusShopTwoCost;
+        document.getElementById("clicks").innerHTML = clicks;
+        document.getElementById("plusAdderTwo").innerHTML = plusAdderTwo;
+    }
+}
+
+// win game function
+function winGame() {
+    if (clicks >= 100000) {
+        endGame = 0;
     }
 }
